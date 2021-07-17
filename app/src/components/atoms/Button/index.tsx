4fa89f1df530284eb7e6e2d498c4473189aac91b
hirokikojima <h.kojima.20140419@gmail.com>
2021-07-18 00:10:06 +0900

    add button component

---
+++ app/src/components/atoms/Button/index.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/display-name */
+import React, { FC, HTMLProps, PropsWithChildren } from 'react'
+import classNames from 'classnames'
+import styles from './styles.module.scss'
+
+export type Props = PropsWithChildren<{
+  type?: 'button' | 'submit' | 'reset'
+}> & HTMLProps<HTMLButtonElement>
+
+const Button: FC<Props> = (props: Props) => {
+  const {
+    children,
+    className,
+    ...rest
+  } = props
+
+  return (
+    <button className={classNames([styles.button, className])} {...rest}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
+
+const buttonFactory = (className: string) => (props: Props) => {
+  return (
+    <Button { ...props } className={styles[className]} />
+  )
+}
+
+export const DefaultButton = buttonFactory('button_default')
+export const SuccessButton = buttonFactory('button_success')
+export const WarningButton = buttonFactory('button_warning')
+export const DangerButton = buttonFactory('button_danger')
