6a51e368afbea585002fd578c5eaf58dc2cf9e81
hirokikojima <h.kojima.20140419@gmail.com>
2021-08-05 00:26:47 +0900

    アイコンコミット漏れ

--- app/src/components/atoms/Icon/index.stories.tsx
+++ app/src/components/atoms/Icon/index.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Story, Meta } from '@storybook/react';
 
-import Icon, { IconProps as Props, AlarmIcon, ChevronRightIcon, PlayIcon, RepeatIcon, Repeat2Icon, StopIcon, TrashCanIcon, TrashCan2Icon } from './index';
+import Icon, { IconProps as Props, AlarmIcon, ChevronRightIcon, PlayIcon, PlusIcon, RepeatIcon, Repeat2Icon, StopIcon, TrashCanIcon, TrashCan2Icon } from './index';
 
 export default {
   title: 'Example/Icon',
@@ -12,6 +12,7 @@ export default {
 export const Alarm: Story<Props> = () => <AlarmIcon />
 export const ChevronRight: Story<Props> = () => <ChevronRightIcon />
 export const Play: Story<Props> = () => <PlayIcon />
+export const Plus: Story<Props> = () => <PlusIcon />
 export const Repeat: Story<Props> = () => <RepeatIcon />
 export const Repeat2: Story<Props> = () => <Repeat2Icon />
 export const Stop: Story<Props> = () => <StopIcon />
